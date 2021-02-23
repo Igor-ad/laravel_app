b52331de8086497c570d11f9eacf51f9ce0355d7
Igor-ad <autodoctor.odessa@gmail.com>
2021-02-23 14:55:10 +0200

    refactoring Router web.php

--- routes/web.php
+++ routes/web.php
@@ -18,34 +18,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/categories', [CategoryController::class, 'index'])
-    ->middleware(['auth'])->name('categories');
-Route::get('/categories/create', [CategoryController::class, 'create'])
-    ->middleware(['auth'])->name('category.create');
-Route::get('/categories/delete/{category}', [CategoryController::class, 'delete'])
-    ->middleware(['auth'])->name('category.delete');
-Route::get('/categories/edit/{category}', [CategoryController::class, 'edit'])
-    ->middleware(['auth'])->name('category.edit');
-Route::get('/categories/{category}', [CategoryController::class, 'show'])
-    ->middleware(['auth'])->name('category.show');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('category.create');
+    Route::get('/categories/delete/{category}', [CategoryController::class, 'delete'])->name('category.delete');
+    Route::get('/categories/edit/{category}', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('category.show');
+
+    Route::get('/products', [ProductController::class, 'index'])->name('products');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('product.create');
+    Route::get('/products/delete/{product}', [ProductController::class, 'delete'])->name('product.delete');
+    Route::get('/products/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::get('/products/{product}', [ProductController::class, 'show'])->name('product.show');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
 Route::post('/categories', [CategoryController::class, 'store'])->name('category.store');
 Route::post('/categories/{category}', [CategoryController::class, 'update'])->name('category.update');
 
-Route::get('/products', [ProductController::class, 'index'])
-    ->middleware(['auth'])->name('products');
-Route::get('/products/create', [ProductController::class, 'create'])
-    ->middleware(['auth'])->name('product.create');
-Route::get('/products/delete/{product}', [ProductController::class, 'delete'])
-    ->middleware(['auth'])->name('product.delete');
-Route::get('/products/edit/{product}', [ProductController::class, 'edit'])
-    ->middleware(['auth'])->name('product.edit');
-Route::get('/products/{product}', [ProductController::class, 'show'])
-    ->middleware(['auth'])->name('product.show');
 Route::post('/products', [ProductController::class, 'store'])->name('product.store');
 Route::post('/products/{product}', [ProductController::class, 'update'])->name('product.update');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
